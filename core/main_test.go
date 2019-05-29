ee6a154efffed2e48c9bfe56c983b9a46f3f1bc7
skar404 <skar404@gmail.com>
2019-05-29 23:10:53 +0300

    ...

--- core/main_test.go
+++ core/main_test.go
@@ -1,24 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/labstack/echo"
 )
 
 func TestMain(m *testing.M) {
-	// Setup
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetPath("/users/:email")
-
-	if assert.NoError(t, h.getUser(c)) {
-		text := rec.Body.String()
-		fmt.Print(text)
-	}
 }
