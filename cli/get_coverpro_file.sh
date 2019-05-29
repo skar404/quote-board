7ca4d1534589ecc1ad1ddf3029806979051b41e8
skar404 <skar404@gmail.com>
2019-05-30 00:34:04 +0300

    update

--- cli/get_coverpro_file.sh
+++ cli/get_coverpro_file.sh
@@ -4,5 +4,7 @@ set -e
 
 go test ./... -v -covermode=count -coverprofile=coverage.out
 
+echo coverage.out > coverage.txt
+
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
 goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
